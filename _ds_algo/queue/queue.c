9eb50c7fe4eb5c28090ac2ca1461de8990088dd2
n2 h9 <n2h9z4@gmail.com>
2022-09-25 19:52:42 +0200

    updated c queue: generalised value container

--- _ds_algo/queue/queue.c
+++ _ds_algo/queue/queue.c
@@ -1,24 +1,47 @@
 #include <stdlib.h>
 
+// pointer to the value which is stored in a queue
+typedef void * qnvalue;
+// ----
+
+
+// ----
+// queue node struct
+// encapsulate node
+
 struct _qnode {
-    int value;
+    qnvalue value;
     struct _qnode * next;
 };
 typedef struct _qnode qnode;
+
+qnode * new_q_node(qnvalue value) {
+    qnode* node = (qnode *)malloc(sizeof(qnode));
+    node->value = value;
+
+    return node;
+}
+
+// end of queue node struct
+// ----
+
+// ----
+// queue
+// all bellow relate to queue
 
 typedef struct {
     qnode * head;
     qnode * tail;
 } queue;
 
-queue * newQueue() {
+queue * new_queue() {
     queue * q = (queue *)malloc(sizeof(queue));
     q->head = NULL;
     q->tail = NULL;
     return q;
 }
 
-unsigned char q_isEmpty(queue * q) {
+unsigned char q_is_empty(queue * q) {
     if (q->head == NULL) {
         return 1;
     }
@@ -27,7 +50,7 @@ unsigned char q_isEmpty(queue * q) {
 
 void q_push(queue * q, qnode * node) {
     node->next = NULL;
-    if (q_isEmpty(q)) {
+    if (q_is_empty(q)) {
         q->head = node;
         q->tail = node;
         return ;
@@ -38,9 +61,9 @@ void q_push(queue * q, qnode * node) {
 }
 
 void q_pop(queue * q) {
-    qnode * oldHead = q->head;
+    qnode * old_head = q->head;
     q->head = q->head->next;
-    oldHead->next = NULL;
+    old_head->next = NULL;
 
     if (q->head == NULL) {
         q->tail = NULL;
@@ -50,13 +73,4 @@ void q_pop(queue * q) {
 // do not check on empty queue
 qnode * q_top(queue * q) {
     return q->head;
-}
-
-
-qnode * newQNode(int value) {
-    qnode* node = (qnode *)malloc(sizeof(qnode));
-    node->value = value;
-    node->next = NULL;
-
-    return node;
 }
---
+++ _ds_algo/queue/queue_usage_test.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <assert.h>
+#include "./queue.c"
+
+// ---
+// - define a struct container for a value
+// - path the pointer to a container struct to new_q_node as a (void *)
+//
+// 
+// Usage example:
+//
+struct _value_c {
+    int i, j, t;
+};
+typedef struct _value_c value_c;
+
+value_c * new_value_c(int i, int j, int t) {
+    value_c* v = (value_c *) malloc(sizeof(value_c));
+    v->i = i;
+    v->j = j;
+    v->t = t;
+
+    return v;
+}
+// 
+
+void testCase0() {
+    int i = 2, j = 4, t = 8;
+
+    value_c * vc = new_value_c(i, j, t);
+    qnode * qn = new_q_node((qnvalue) vc);
+    
+    queue * q = new_queue();
+    q_push(q, qn);
+
+    qnode * qn0 = q_top(q);
+    value_c * vc0 = (value_c *) qn0->value;
+    q_pop(q);
+
+    assert(i == vc0->i);
+    assert(j == vc0->j);
+    assert(t == vc0->t);
+
+    free(vc0);
+    free(qn0);
+    free(q);
+}
+
+
+int main() {
+    testCase0();
+    printf("ok\n");
+
+    return 0;
+}
