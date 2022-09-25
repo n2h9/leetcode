5729ad5622b152943a28567faddf35fcaeff3206
n2 h9 <n2h9z4@gmail.com>
2022-09-25 19:54:35 +0200

    rotting-oranges: c: queue updates

--- problems/rotting-oranges/c/queue.c
+++ problems/rotting-oranges/c/queue.c
@@ -1,30 +1,45 @@
 #include <stdlib.h>
 
-struct _qnvalue {
-    int i, j;
-    int t;
-};
-typedef struct _qnvalue qnvalue;
+// ----
+// queue node struct
+// encapsulate node
+
+// pointer to the value which is stored in a queue
+typedef void* qnvalue;
 
 struct _qnode {
-    qnvalue * value;
+    qnvalue value;
     struct _qnode * next;
 };
 typedef struct _qnode qnode;
+
+qnode * new_q_node(qnvalue value) {
+    qnode* node = (qnode *)malloc(sizeof(qnode));
+    node->value = value;
+
+    return node;
+}
+
+// end of queue node struct
+// ----
+
+// ----
+// queue
+// all bellow relate to queue
 
 typedef struct {
     qnode * head;
     qnode * tail;
 } queue;
 
-queue * newQueue() {
+queue * new_queue() {
     queue * q = (queue *)malloc(sizeof(queue));
     q->head = NULL;
     q->tail = NULL;
     return q;
 }
 
-unsigned char q_isEmpty(queue * q) {
+unsigned char q_is_empty(queue * q) {
     if (q->head == NULL) {
         return 1;
     }
@@ -33,7 +48,7 @@ unsigned char q_isEmpty(queue * q) {
 
 void q_push(queue * q, qnode * node) {
     node->next = NULL;
-    if (q_isEmpty(q)) {
+    if (q_is_empty(q)) {
         q->head = node;
         q->tail = node;
         return ;
@@ -44,9 +59,9 @@ void q_push(queue * q, qnode * node) {
 }
 
 void q_pop(queue * q) {
-    qnode * oldHead = q->head;
+    qnode * old_head = q->head;
     q->head = q->head->next;
-    oldHead->next = NULL;
+    old_head->next = NULL;
 
     if (q->head == NULL) {
         q->tail = NULL;
@@ -56,21 +71,4 @@ void q_pop(queue * q) {
 // do not check on empty queue
 qnode * q_top(queue * q) {
     return q->head;
-}
-
-
-qnode * newQNode(qnvalue* value) {
-    qnode* node = (qnode *)malloc(sizeof(qnode));
-    node->value = value;
-
-    return node;
-}
-
-qnvalue * newQNValue(int i, int j, int t) {
-    qnvalue* value = (qnvalue *) malloc(sizeof(qnvalue));
-    value->i = i;
-    value->j = j;
-    value->t = t;
-
-    return value;
 }
--- problems/rotting-oranges/c/solution.c
+++ problems/rotting-oranges/c/solution.c
@@ -2,18 +2,38 @@
 #include <stdio.h>
 #include "queue.c"
 
+// ----
+// _value_c is a value container, holds the value of node in queue
+
+struct _value_c {
+    int i, j;
+    int t;
+};
+typedef struct _value_c value_c;
+
+value_c * new_value_c(int i, int j, int t) {
+    value_c* v = (value_c *) malloc(sizeof(value_c));
+    v->i = i;
+    v->j = j;
+    v->t = t;
+
+    return v;
+}
+
+// ----
+
 #define ORANGE 1
 #define ORANGE_ROTTEN 2
 
 int orangesRotting(int** grid, int gridSize, int* gridColSize) {
-    queue * q = newQueue();
+    queue * q = new_queue();
     
     // total number of orranges
     int num = 0;
     for (int i = 0; i < gridSize; i++) {
         for (int j = 0; j < *gridColSize; j++) {
             if (grid[i][j] == ORANGE_ROTTEN) {
-                q_push(q, newQNode(newQNValue(i, j, 0)));
+                q_push(q, new_q_node((void *)new_value_c(i, j, 0)));
                 num++;
             } else if (grid[i][j] == ORANGE) {
                 num++;
@@ -26,28 +46,28 @@ int orangesRotting(int** grid, int gridSize, int* gridColSize) {
 
     // current time tick
     int time = 0;
-    for (;!q_isEmpty(q);) {
+    for (;!q_is_empty(q);) {
         qnode * node = q_top(q);
-        qnvalue * value = node->value;
+        value_c * v = (value_c *)node->value;
         q_pop(q);
 
-        time = value->t;
+        time = v->t;
         rnum++;
 
-        int i = value->i, j = value->j;
+        int i = v->i, j = v->j;
         // possible coordinates in grid which could become rotten
         int c[4][2] = {{i,j-1}, {i,j+1},{i-1,j},{i+1,j}};
         for (int k = 0; k < 4; k++) {
             if (c[k][0] >= 0 && c[k][0] < gridSize && c[k][1] >= 0 && c[k][1] < *gridColSize) {
                 if (grid[c[k][0]][c[k][1]] == ORANGE) {
-                    q_push(q, newQNode(newQNValue(c[k][0], c[k][1], time+1)));
+                    q_push(q, new_q_node((void *)new_value_c(c[k][0], c[k][1], time+1)));
                     grid[c[k][0]][c[k][1]] = ORANGE_ROTTEN;
                 }
             }
         }
 
 
-        free(value);
+        free(v);
         free(node);
     }
 
