29d5c875ae78948d26511e1653c56aa5fea00f12
n2 h9 <n2h9z4@gmail.com>
2024-10-13 14:23:30 +0200

    cpp: adjustments in cpp templ

--- problems/_templ_cpp/cpp/solution_test.cpp
+++ problems/_templ_cpp/cpp/solution_test.cpp
@@ -6,7 +6,7 @@ TEST(SolutionTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
 
-  auto solution = Solution();
+  auto solution = Solution{};
 
   EXPECT_EQ(42, solution.solve(40, 2));
 }
