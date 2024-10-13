7993a36088615eb2d1e4ca1b6fde172a422a12b0
n2 h9 <n2h9z4@gmail.com>
2024-10-13 12:01:34 +0200

    cpp: add cpp template and .clang-format

---
+++ problems/_templ_cpp/cpp/solution.cpp
@@ -0,0 +1,6 @@
+using namespace ::std;
+
+class Solution {
+ public:
+  int solve(int a, int b) { return a + b; }
+};
---
+++ problems/_templ_cpp/cpp/solution_test.cpp
@@ -0,0 +1,12 @@
+#include "solution.cpp"
+
+#include <gtest/gtest.h>
+
+TEST(SolutionTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+
+  auto solution = Solution();
+
+  EXPECT_EQ(42, solution.solve(40, 2));
+}
